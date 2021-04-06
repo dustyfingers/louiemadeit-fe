70582d3bb549c70ead031091f5d83420cefb76c2
lou <dustyfingers01257@gmail.com>
2021-04-06 16:54:13 -0600

    built cart counter component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -8,7 +8,7 @@ import { setCurrentUser } from '../../redux/user/user-actions';
 
 // TODO: sign in should change based on the user state
 
-const Menu = ({ history, currentUser, dispatch }) => {
+const Menu = ({ history, currentUser, cartItems, dispatch }) => {
     return (
     <>
         <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="TopMenu">
@@ -32,8 +32,9 @@ const Menu = ({ history, currentUser, dispatch }) => {
                                 <span className="nav-link" onClick={() => dispatch(setCurrentUser(null))}>sign out.</span>
                             </li>
                         }
-                        <li className="nav-item">
-                            <span className="nav-link" onClick={() => history.push("/cart")}><img src="/cart.svg" height='32px' width='32px' alt="shopping cart"/></span>
+                        <li className="nav-item cart-counter-wrapper">
+                            <span className="nav-link" onClick={() => history.push("/cart")}><img src="/cart.svg" height='24px' width='24px' alt="shopping cart"/></span>
+                            {cartItems.length > 0 && (<span className="cart-counter">{cartItems.length}</span>)}
                         </li>
                     </ul>
                 </div>
@@ -44,7 +45,7 @@ const Menu = ({ history, currentUser, dispatch }) => {
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser,
-    cartHidden: state.cart.hidden
+    cartItems: state.cart.cartItems
 });
 
 export default connect(mapStateToProps)(withRouter(Menu));
--- src/redux/cart/cart-reducer.js
+++ src/redux/cart/cart-reducer.js
@@ -7,11 +7,6 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden
-            }
         case CartActionTypes.ADD_CART_ITEM:
             return {
                 ...state,
