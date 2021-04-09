1e8049ffcf12ee2b695c5a1cf2ddfb455d0dce99
lou <dustyfingers01257@gmail.com>
2021-04-09 12:58:39 -0600

    fixed first major performance issue

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -5,18 +5,16 @@ import { removeCartItem } from '../../redux/cart/cart-actions';
 
 const CartItem = ({item, dispatch}) => {
     return (
-        <div>
-            <div className="card border-light mb-3">
-                <div className="row g-0">
-                    <div className="card-header col-md-4">Header</div>
-                    <div className="card-body col-md-8 d-flex">
-                        <div>
-                            <h5 className="card-title">{item.trackName} - {item.prices.leasePriceStems}</h5>
-                            <p className="card-text">{item.meta.description}</p>
-                        </div>
-                        <div className="d-flex align-items-center justify-content-end">
-                            <img src="/x-square.svg" height='24px' width='24px' alt={`remove ${item.trackName} from cart`} onClick={() => dispatch(removeCartItem(item))}/>
-                        </div>
+        <div className="card border-light mb-3">
+            <div className="row g-0">
+                <div className="card-header col-md-4">Header</div>
+                <div className="card-body col-md-8 d-flex">
+                    <div>
+                        <h5 className="card-title">{item.trackName} - {item.prices.leasePriceStems}</h5>
+                        <p className="card-text">{item.meta.description}</p>
+                    </div>
+                    <div className="d-flex align-items-center justify-content-end">
+                        <img src="/x-square.svg" height='24px' width='24px' alt={`remove ${item.trackName} from cart`} onClick={() => dispatch(removeCartItem(item))}/>
                     </div>
                 </div>
             </div>
--- src/components/TrackPreview/TrackPreview.jsx
+++ src/components/TrackPreview/TrackPreview.jsx
@@ -8,66 +8,34 @@ import { setCurrentTrack } from '../../redux/player/player-actions';
 import { addCartItem } from '../../redux/cart/cart-actions';
 import { apiLink } from '../../env';
 
-// TODO: make playIcon change back when different track is played
+// TODO: make playIcon change back when different track is played or when clicked again (to pause)
 const TrackPreview = ({track, cartItems, currentPlayerTrack, dispatch}) => {
-    const [coverUrl, setCoverUrl] = useState('');
-    const [trackUrl, setTrackUrl] = useState('');
     const [isPlaying, setIsPlaying] = useState(false);
     const [playIcon, setPlayIcon] = useState('/play-btn.svg');
-
-    useEffect(() => {
-        const fetchTrackData = async () => {
-            try {
-                const url = apiLink + '/s3/generate-get-url';
-    
-                const trackFileOptions = {
-                    params: {
-                        Key: track.taggedVersion
-                    }
-                };
-    
-                const coverArtOptions = {
-                    params: {
-                        Key: track.coverArt
-                    }
-                };
-        
-                const trackRes = await axios.get(url, trackFileOptions);
-                const coverArtRes = await axios.get(url, coverArtOptions);
-                setCoverUrl(coverArtRes.data.getUrl);
-                setTrackUrl(trackRes.data.getUrl);
-            } catch (err) {
-                console.log(err);
-            }
-        }
-
-        fetchTrackData();
-    }, []);
 
     // this watches the global track being played and sets local state accordingly
     useEffect(() => {
         if (currentPlayerTrack === null && isPlaying) setPlayIcon('/play-btn-fill.svg');
-        else if (trackUrl === currentPlayerTrack) setPlayIcon('/play-btn-fill.svg');
-        else if (trackUrl !== currentPlayerTrack) setPlayIcon('/play-btn.svg');
-
+        else if (track.taggedVersionUrl === currentPlayerTrack) setPlayIcon('/play-btn-fill.svg');
+        else if (track.taggedVersionUrl !== currentPlayerTrack) setPlayIcon('/play-btn.svg');
     }, [currentPlayerTrack]);
 
-    const handleClickPlayButton = evt => {
+    const handleClickPlayButton = () => {
         setIsPlaying(true);
-        dispatch(setCurrentTrack(trackUrl));
+        dispatch(setCurrentTrack(track.taggedVersionUrl));
     };
 
     const handleClickAddToCartButton = evt => {
-        if (!cartItems.includes(track)) {
-            dispatch(addCartItem(track))
-        } else {
+        if (!cartItems.includes(track)) dispatch(addCartItem(track));
+        else {
+            // TODO: error handling here
             console.log('item already in cart!')
         }
     };
 
     return (
         <div className='card col-md-4 col-lg-2 m-3'>
-            <img src={coverUrl} className='card-img-top' alt={`${track.trackName}-cover`} />
+            <img src={track.coverArtUrl} className='card-img-top' alt={`${track.trackName}-cover`} />
             <div className='card-body'>
                 <p className='card-text'>{track.trackName}</p>
                 <div className='track-card-btns d-flex align-items-end justify-content-between'>
