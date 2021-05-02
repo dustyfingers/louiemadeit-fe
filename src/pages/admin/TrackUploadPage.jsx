b3f775dae15f37e46d42ffaf5e452ec371e3980f
lou <dustyfingers01257@gmail.com>
2021-05-02 07:59:55 -0600

    refactored admin structure of routes

--- src/pages/admin/TrackUploadPage.jsx
+++ src/pages/admin/TrackUploadPage.jsx
@@ -1,25 +1,24 @@
 // import libs/other
 import React from "react";
 import { connect } from "react-redux";
-import { Redirect } from "react-router";
+import { withRouter } from "react-router-dom";
 
 // import components
 import TrackUploadForm from "../../components/TrackUploadForm/TrackUploadForm.jsx";
 
-const TrackUploadPage = ({ currentUser }) => {
-    if(currentUser) {
-        return currentUser.isAdmin ?
-            (<div>
-                <h1>Upload a Track</h1>
-                <TrackUploadForm />
-            </div>) : 
-            <Redirect to="/" />;
-    } else { return <Redirect to="/sign-in" />;}
-
+const TrackUploadPage = ({ currentUser, history }) => {
+    if(!currentUser) history.push('/sign-in');
+    if(!currentUser.isAdmin) history.push('/');
+    return (
+        <div>
+            <h1>Upload a Track</h1>
+            <TrackUploadForm />
+        </div>
+    );
 };
 
 const mapStateToProps = state => ({
     currentUser: state.user.currentUser 
 });
 
-export default connect(mapStateToProps)(TrackUploadPage);
+export default connect(mapStateToProps)(withRouter(TrackUploadPage));
