fedbaca2bfbe1189e80e24fc75eac2d039ca5839
lou <dustyfingers01257@gmail.com>
2021-05-13 07:40:19 -0600

    small refactor

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,11 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
-import env from "../env";
+import { env } from "../env";
 
 let middlewares, store;
 
-if (env == "local") {
+if (env === "local") {
     middlewares = [logger];
     store = createStore(rootReducer, applyMiddleware(...middlewares));
 } else store = createStore(rootReducer);
