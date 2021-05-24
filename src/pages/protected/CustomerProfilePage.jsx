e3b26d819051b5799d6ed6d2d8130bfeda043fed
lou <dustyfingers01257@gmail.com>
2021-05-24 13:30:08 -0600

    got purchased tracks showing on customer profile page

--- src/pages/protected/CustomerProfilePage.jsx
+++ src/pages/protected/CustomerProfilePage.jsx
@@ -6,13 +6,12 @@ import { ToastsStore } from 'react-toasts';
 import { apiLink } from '../../env';
 
 const CustomerProfilePage = () => {
-    const [orders, setOrders] = useState([]);
+    const [tracks, setTracks] = useState([]);
 
     const fetchCustomerData = async () => {
         try {
-            const { data } = await axios.get(`${apiLink}/stripe/purchased-tracks`);
-            const results = data.purchasedTracks;
-            setOrders(results);
+            const { data: { purchasedTracks } } = await axios.get(`${apiLink}/stripe/purchased-tracks`);
+            setTracks(purchasedTracks);
         } catch (error) {
             ToastsStore.error('There was an error fetching your profile...');
         }
@@ -24,7 +23,7 @@ const CustomerProfilePage = () => {
         <div className="d-flex flex-column justify-content-center text-center">
             <h1>CUSTOMER PROFILE</h1>
             <div>
-                {orders.map(order => <p key={order.id}>{order.id}</p>)}
+                {tracks.length ? tracks.map((track, idx) => <p key={idx}>{track.trackName}</p>) : 'No tracks purchased.'}
             </div>
         </div>
     );
