b96bcd8861c3b59c7f5b6e54e144e22ace05f6b2
lou <dustyfingers01257@gmail.com>
2021-10-09 15:45:47 -0600

    refactor helper function

--- src/helpers/s3Upload.js
+++ src/helpers/s3Upload.js
@@ -3,16 +3,10 @@ import axios from "axios";
 export default {
     handleUploadToS3: async (file, putUrl) => {
         try {
-            const urlResponse = await axios.get(putUrl, {
-                params: {
-                    Key: file.name,
-                    ContentType: file.type
-                }
-            })
+            const urlResponse = await axios.get(putUrl, { params: { Key: file.name, ContentType: file.type } })
             await axios.put(urlResponse.data.putUrl, file, { headers: { 'Content-Type': file.type } })
         } catch (err) {
-
+            console.log({err})
         }
-
     }
 }
